4350ece42c2b5e8f819c2825d95d40bab6ae6e7e
Mathias Filype <mathiasfilypedev@gmail.com>
2025-06-22 18:06:17 -0300

    global.css e theme.css criados e adicionados componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
+
+import './style/global.css';
+import './style/theme.css';
+
 export function App() {
   console.log('Olá mundo');
 
   return (
-  <div>
-    <h1>Olá mundo</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, sequi ab!
-      Facilis impedit beatae eum distinctio molestiae harum amet ipsa sequi,
-      totam esse! Aut similique excepturi iure fugit libero voluptates?
-      Temporibus voluptatem vero nihil aperiam, ad consequuntur, molestias
-      eveniet architecto, ipsa totam quisquam iste quos! Aliquam ipsa deleniti
-      eos fugit consectetur laboriosam iure. Praesentium vel soluta repellendus
-      veritatis iusto corporis. Pariatur delectus nam perspiciatis quis
-      accusamus qui dolor error eveniet consequuntur? Unde tempore, recusandae
-      quod perspiciatis non, adipisci officia, laboriosam alias sequi voluptatem
-      odio iste et sunt reiciendis dolorum maxime.
-    </p>
-  </div>
+    <>
+      <h1>Olá mundo</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, sequi
+        ab! Facilis impedit beatae eum distinctio molestiae harum amet ipsa
+        sequi, totam esse! Aut similique excepturi iure fugit libero voluptates?
+        Temporibus voluptatem vero nihil aperiam, ad consequuntur, molestias
+        eveniet architecto, ipsa totam quisquam iste quos! Aliquam ipsa deleniti
+        eos fugit consectetur laboriosam iure. Praesentium vel soluta
+        repellendus veritatis iusto corporis. Pariatur delectus nam perspiciatis
+        quis accusamus qui dolor error eveniet consequuntur? Unde tempore,
+        recusandae quod perspiciatis non, adipisci officia, laboriosam alias
+        sequi voluptatem odio iste et sunt reiciendis dolorum maxime.
+      </p>
+    </>
   );
 }
 
