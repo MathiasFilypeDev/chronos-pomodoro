93514f4e441667ed8beb0df94bff9954b3c0f0ec
Mathias Filype <mathiasfilypedev@gmail.com>
2025-06-23 15:02:53 -0300

    Inserindos classes ao componente Heading

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,12 @@
 import styles from './Heading.module.css';
 
+/*export function Heading() {
+  const classes = `${styles.heading} ${styles.yellow}`;
+  console.log(styles);
+  return <h1 className={classes}>Olá mundo</h1>;
+}*/
+
 export function Heading() {
   console.log(styles);
-  return <h1 className={styles.heading}>Olá mundo</h1>
+  return <h1 className={`${styles.heading} ${styles.yellow}`}>Olá mundo</h1>;
 }
