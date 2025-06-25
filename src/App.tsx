95ef96de6ff56d9db2c6de314ad906589f8fc4a6
Mathias Filype <mathiasfilypedev@gmail.com>
2025-06-25 14:02:23 -0300

    adição de algumas configurações settings.json | criação de: /components/logo; /components/menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import { Heading } from './components/Heading';
+import { Container } from './components/container';
+import { Logo } from './components/logo';
+import { Menu } from './components/menu';
+
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  console.log('Olá mundo');
-
   return (
     <>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, sequi
-        ab! Facilis impedit beatae eum distinctio molestiae harum amet ipsa
-        sequi, totam esse! Aut similique excepturi iure fugit libero voluptates?
-        Temporibus voluptatem vero nihil aperiam, ad consequuntur, molestias
-        eveniet architecto, ipsa totam quisquam iste quos! Aliquam ipsa deleniti
-        eos fugit consectetur laboriosam iure. Praesentium vel soluta
-        repellendus veritatis iusto corporis. Pariatur delectus nam perspiciatis
-        quis accusamus qui dolor error eveniet consequuntur? Unde tempore,
-        recusandae quod perspiciatis non, adipisci officia, laboriosam alias
-        sequi voluptatem odio iste et sunt reiciendis dolorum maxime.
-      </p>
+      <Container>
+        <Logo></Logo>
+      </Container>
+      <Container>
+       <Menu />
+      </Container>
     </>
   );
 }
